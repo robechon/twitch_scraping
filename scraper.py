7683d8a9a934ecf0feb6b950bd28ef7a1e07366a
rbechon <robinbechon42@gmail.com>
2020-03-02 14:34:57 +0100

    Add function to get ressource type (clips or videos).
    Signed-off-by: rbechon <robinbechon42@gmail.com>

--- scraper.py
+++ scraper.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 
-def get_url():
-    base_url = 'www.twitch.tv/'
-    channel_name = input("Please enter the channel name you want to scrape: ")
+BASE_URL = 'www.twitch.tv/'
+
+def get_channel_name():
+    channel_name = input('Please enter the channel name you want to scrape: ')
     if channel_name.isalnum() and len(channel_name) >= 3 and len(channel_name) < 25: #Replace by a regEX
-        return base_url + channel_name
+        return BASE_URL + channel_name
     print('Incorrect username. Stop.')
     return None
 
-
-if __name__ == "__main__":
-    get_url()
+def get_ressource_type():
+    ressource = input('Please make a choice between:\n1. Clips\n2. Videos\nYour choice: ')
+    if ressource == '1':
+        ressource = '/clips?filter=clips'
+    elif ressource == '2':
+        ressource = '/videos'
+    else:
+        print('Incorrect input. Stop.')
+        return None
+    return ressource
